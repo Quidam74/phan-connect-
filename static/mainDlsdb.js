4b8d2985879b5a17492c4ad7148a80d1ead89567
Unknown <toutlemonde74@gmail.com>
2018-12-04 10:50:06 +0100

    modif apelle get + exemple

--- static/mainDlsdb.js
+++ static/mainDlsdb.js
@@ -1,21 +1,27 @@
 
 $(document).ready(function(){
-  function getTemp(){
-    $.ajax({
-      url: 'https://dans-la-salle-de-bains.eu-gb.mybluemix.net/api/temperature',
-      dataType: 'json',
-      type:'GET',
-      headers: {
-        'accept': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
-      },
-      success: function(data) {
-        console.log(data)
-      }
-    })
-  }
-
+  console.log(getLastData());
   getTemp();
 });
+function getLastData(){
+  $.get( "https://dans-la-salle-de-bains.eu-gb.mybluemix.net/api/value/last", function(data) {
+      return data
+    })
+}
+
+function getTemp(){
+  $.ajax({
+    url: 'https://dans-la-salle-de-bains.eu-gb.mybluemix.net/api/temperature',
+    dataType: 'json',
+    type:'GET',
+    headers: {
+      'accept': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
+    },
+    success: function(data) {
+      console.log(data)
+    }
+  })
+}
