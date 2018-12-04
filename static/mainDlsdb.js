cd0474a1bbd3f263d21736535704ce25f7bfc220
Unknown <mx.girard42@gmail.com>
2018-12-04 14:40:30 +0100

    Auto stash before merge of "master" and "origin/master"

--- static/mainDlsdb.js
+++ static/mainDlsdb.js
@@ -1,10 +1,3 @@
-$(document).ready(function () {
-    getListData();
-    setInterval(() => {
-        getLastData();
-        console.log("change")
-    }, 5000)
-
 $(document).ready(function(){
   // test();
   getListData();
@@ -14,11 +7,6 @@ $(document).ready(function(){
     getLastData();
   },5000)
 });
-// function getLastData(){
-//   $.get( "https://dans-la-salle-de-bains.eu-gb.mybluemix.net/api/value/last", function(data) {
-//       return data
-//     })
-// }
 
 function getListData(){
   $.ajax({
@@ -52,37 +40,24 @@ function getLastData(){
     }
   })
 }
-// function test(){
-//   data = [{temperature:"29"},{temperature:"35"},{temperature:"35"},{temperature:""},{temperature:"0"},{temperature:"0"},{temperature:"35"},{temperature:"35"}]
-//   var i = 0;
-//   var svgns = "http://www.w3.org/2000/svg"
-//   var container = document.querySelector('.graphique > svg > g');
-//   data.forEach(function(data){
-//     let circle = document.createElementNS(svgns,'circle');
-//     circle.setAttribute("r",5);
-//     circle.setAttribute("cx",i);
-//     circle.setAttribute("cy",80-data.temperature)
-//     circle.setAttribute('style', 'color:black');
-//     container.appendChild(circle);
-//     console.log(circle);
-//     i=i+5;
-//   })
-// }
+
 function listData(data){
-  var i = 0;
   var values = data.values;
   var svgns = "http://www.w3.org/2000/svg"
   var container = document.querySelector('.graphique > svg > g');
-  values.forEach(function(value){
+  var x = 0;
+  var lenValues = values.length+1;
+  var i = lenValues-42;
+  for (i; i <= lenValues; i++) {
     let circle = document.createElementNS(svgns,'circle');
     circle.setAttribute("r",5);
-    circle.setAttribute("cx",i);
-    circle.setAttribute("cy",84-value.temperature)
+    circle.setAttribute("cx",x);
+    circle.setAttribute("cy",84-values[i].temperature)
     circle.classList.add('circle');
+    console.log(i);
     container.appendChild(circle);
-    console.log(circle);
-    i=i+25;
-  })
+    x=x+25;
+  }
 }
 //recupère et stocke dans div dernière données
 function lastData(data){
@@ -90,15 +65,10 @@ function lastData(data){
   let dewValue = document.querySelector('span.dew-value');
   let humValue = document.querySelector('span.hum-value');
 
+  tempValue.innerHTML = Math.floor(data.temperature*10)/10+"°C";
+  dewValue.innerHTML = Math.floor(data.dew_point*10)/10+"°C";
+  humValue.innerHTML = Math.floor(data.humidity*10)/10+"%";
 
-
-    tempValue.innerHTML = Math.floor(data.temperature*10)/10+"°C";
-    dewValue.innerHTML = Math.floor(data.dew_point*10)/10+"°C";
-    humValue.innerHTML = Math.floor(data.humidity*10)/10+"%";
-
-  //let divCurrentDate = document.querySelector('div.time-value');
-  //var d = new Date(Date.parse(data.timestamp));
-  //divCurrentDate.innerHTML = 'le : ' + d.getDate() + '/' + (d.getMonth()+1) + '/' + d.getFullYear() + ' à ' + d.getHours() + ':' + d.getMinutes();
     if (eval(data.temperature) >= 30) {
         tempValue.classList.add("rouge");
     } else if (eval(data.temperature) <= 15){
@@ -106,12 +76,5 @@ function lastData(data){
     } else {
     tempValue.classList.remove("bleu");
     tempValue.classList.remove("rouge");
-
     }
-
-
-    //let divCurrentDate = document.querySelector('div.time-value');
-    //var d = new Date(Date.parse(data.timestamp));
-    //divCurrentDate.innerHTML = 'le : ' + d.getDate() + '/' + (d.getMonth()+1) + '/' + d.getFullYear() + ' à ' + d.getHours() + ':' + d.getMinutes();
-
 }
